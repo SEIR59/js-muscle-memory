32b79cf547fb6d649de6476357eda26c815534f7
peterxu35 <peter.px.xu@gmail.com>
2022-05-18 17:59:07 -0400

    Question 3 finished

--- 3-is-prime.js
+++ 3-is-prime.js
@@ -12,6 +12,14 @@ isPrime(2017); // => true
 ***************************************************************************/
 
 function isPrime(number) {
-
+    if (number === 1){
+        return false
+    }
+    for (let i = 2; i < number; i++){
+        if (number % i === 0){
+            return false
+        }
+    }
+    return true
 }
 
