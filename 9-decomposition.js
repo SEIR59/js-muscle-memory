541989e2f7e6a889d63ecf67b821da78aab5a7b0
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-18 15:20:14 -0700

    9 done

--- 9-decomposition.js
+++ 9-decomposition.js
@@ -19,18 +19,18 @@ isPrime(303212); // => false
 ***********************************************************************/
 
 function isPrime(number) {
-
+    if(number ===2){return true}
     //check if the number is divisible by 2. Apparently I can't use 'return' in this format.
     let checker = (number % 2 === 0 ? true : false)
     //if it's even, it's definitely not prime.
     if (checker){return false}
 
-    //make a list of odd numbers between 3 and number-1/2
+    //make a list of odd numbers between 3 and number-1/2. Do you know a better way to generate a list of numbers? Not just [0,1,2...] but adjust them, [3,4,5]?
     let oddArr = Array.from(Array(Math.floor((number - 2) / 2)).keys()).map((x) => x + 2).filter((x) => x%2 == 1)
     //if one of those numbers has mod 0, it is not prime
     return (oddArr.map((x) => number%x).includes(0) == false)
 }
-console.log(isPrime(19))
+console.log( '19' , isPrime(19))
 /***********************************************************************
 Using the `isPrime` function you made, write a function `firstNPrimes(n)`
 that returns an array of the first `n` prime numbers.
@@ -44,14 +44,14 @@ firstNPrimes(4); // => [2, 3, 5, 7]
 
 function firstNPrimes(n) {
     const nPrimes = []
-    let j = 3
+    let j = 2
     while (nPrimes.length<n){
         isPrime(j)? nPrimes.push(j) : false
         j++
     }
     return nPrimes
 }
-console.log(firstNPrimes(5))
+console.log('checking 5' ,firstNPrimes(5))
 
 /***********************************************************************
  Using `firstNPrimes`, write a function `sumOfNPrimes(n)` that returns
@@ -63,8 +63,8 @@ sumOfNPrimes(0); // => 0
 sumOfNPrimes(1); // => 2
 sumOfNPrimes(4); // => 17
 ***********************************************************************/
-
+//i think the example 
 function sumOfNPrimes(n) {
     return firstNPrimes(n).reduce((x,y)=>x+y)
 }
-console.log(sumOfNPrimes(5))
+console.log('checking 400' , sumOfNPrimes(400))
