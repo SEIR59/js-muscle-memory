7497d1f3e7751a74f1991390ff17ff27962c30d7
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-05-18 19:18:49 -0400

    finish 4

--- 4-cat-builder.js
+++ 4-cat-builder.js
@@ -11,6 +11,26 @@ const cat2 = catBuilder('Whiskers', 'rainbow', ['poptarts']);
 cat2; // => { name: 'Whiskers', color: 'rainbow', toys: [ 'poptarts' ] }
 ***********************************************************************/
 
+///need to use "new"
 function catBuilder(name, color, toys) {
-
+  this.name = name;
+  this.color = color;
+  this.toys = toys;
 }
+
+const cat1 = new catBuilder("Garfield", "golden", ["scratching-post", "yarn"]);
+console.log(cat1);
+
+///do not need to use "new"
+function catBuilderAlt(name, color, toys) {
+  // Define desired object
+  var obj = {
+    name: name,
+    color: color,
+    toys: toys,
+  };
+  return obj;
+}
+
+const cat2 = catBuilderAlt("Whiskers", "rainbow", ["poptarts"]);
+console.log(cat2);
