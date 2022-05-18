de2157aac9c07bdcc9e10aa16251ef4e023f5222
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-05-18 19:35:09 -0400

    9-decomposition complete

--- 9-decomposition.js
+++ 9-decomposition.js
@@ -17,11 +17,16 @@ isPrime(1693); // => true
 isPrime(15); // => false
 isPrime(303212); // => false
 ***********************************************************************/
-
-function isPrime(number) {
-    
-}
+const isPrime = number => {
+    for(let i = 2; i < number; i++)
+      if(number % i === 0) return false;
+    return number > 1;
+};
   
+// console.log(isPrime(2)); // => true
+// console.log(isPrime(1693)); // => true
+// console.log(isPrime(15)); // => false
+// console.log(isPrime(303212)); // => false
 /***********************************************************************
 Using the `isPrime` function you made, write a function `firstNPrimes(n)`
 that returns an array of the first `n` prime numbers.
@@ -33,10 +38,19 @@ firstNPrimes(1); // => [2]
 firstNPrimes(4); // => [2, 3, 5, 7]
 ***********************************************************************/
 
-function firstNPrimes(n) {
-
+const firstNPrimes = n => {
+    let primesArray = [], iterator = 0;
+    while(primesArray.length < n) {
+        if(isPrime(iterator)){
+            primesArray = [...primesArray, iterator]
+        }
+        iterator++;
+    }
+    return primesArray;
 }
-
+// console.log(firstNPrimes(0)); // => []
+// console.log(firstNPrimes(1)); // => [2]
+// console.log(firstNPrimes(4)); // => [2, 3, 5, 7]
 /***********************************************************************
  Using `firstNPrimes`, write a function `sumOfNPrimes(n)` that returns
 the sum of the first `n` prime numbers.
@@ -47,7 +61,13 @@ sumOfNPrimes(0); // => 0
 sumOfNPrimes(1); // => 2
 sumOfNPrimes(4); // => 17
 ***********************************************************************/
-
-function sumOfNPrimes(n) {
-
+const sumOfNPrimes = n => {
+    let primeArray = firstNPrimes(n);
+    let sum = 0;
+    primeArray.forEach(prime => sum += prime)
+    return sum;
 }
+
+console.log(sumOfNPrimes(0)); // => 0
+console.log(sumOfNPrimes(1)); // => 2
+console.log(sumOfNPrimes(4)); // => 17
