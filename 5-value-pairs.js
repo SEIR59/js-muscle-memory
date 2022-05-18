dd9816edc3372549ce16ac926d778f6ea8b9cf71
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-18 19:02:23 -0400

    5.-value-pairs

--- 5-value-pairs.js
+++ 5-value-pairs.js
@@ -4,14 +4,16 @@ and a key (string). The function should return an array containing the
 corresponding values of the objects for the given key.
 
 Examples:
-
+*/
 const object1 = {name: 'One', location: 'Remote', age: 1};
 const object2 = {name: 'Two', location: 'San Francisco'};
 
-valuePair(object1, object2, 'location'); // => [ 'Remote', 'San Francisco' ]
-valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
-***********************************************************************/
-
 function valuePair(obj1, obj2, key) {
-
+    let array = []
+    //can't access through dot notation it has to be bracket because key is a variable.
+    array.push(obj1[key])
+    array.push(obj2[key])
+    return array
 }
+console.log(valuePair(object1, object2, 'location')); // => [ 'Remote', 'San Francisco' ]
+//console.log(valuePair(object2, object2, 'name')); // => [ 'One', 'Two' ]
