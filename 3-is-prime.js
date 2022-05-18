18266cf6eca5d04e2d7ee20597777f8cab0340bd
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-18 16:22:48 -0700

    Redid 3

--- 3-is-prime.js
+++ 3-is-prime.js
@@ -10,16 +10,25 @@ isPrime(11); // => true
 isPrime(9); // => false
 isPrime(2017); // => true
 ***************************************************************************/
-
+function oddRange(end) {
+    let solution = []
+    let i = 1
+    while(i<end){
+        solution.push(i)
+        i+=2
+    }
+    return solution
+}
 function isPrime(number) {
     if(number ===2){return true}
-    //check if the number is divisible by 2. Apparently I can't use 'return' in this format.
+    
+    //if it's even, it's definitely not prime, besides 2
     let checker = (number % 2 === 0 ? true : false)
-    //if it's even, it's definitely not prime.
     if (checker){return false}
 
-    //make a list of odd numbers between 3 and number-1/2. Do you know a better way to generate a list of numbers? Not just [0,1,2...] but adjust them, [3,4,5]?
-    let oddArr = Array.from(Array(Math.floor((number - 2) / 2)).keys()).map((x) => x + 2).filter((x) => x%2 == 1)
+    //used js-2 function to generate odd range. If it contained an even factor, %2 would have found it.
+    let oddArr = oddRange((number-1)/2)
+
     //if one of those numbers has mod 0, it is not prime
     return (oddArr.map((x) => number%x).includes(0) == false)
 }
