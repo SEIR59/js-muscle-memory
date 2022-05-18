ee6b7e23f01e7975fbfde11b76399613e4020785
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-05-18 19:06:28 -0400

    finish 3

--- 3-is-prime.js
+++ 3-is-prime.js
@@ -11,7 +11,20 @@ isPrime(9); // => false
 isPrime(2017); // => true
 ***************************************************************************/
 
+///code souce is from:https://www.programiz.com/javascript/examples/prime-number#:~:text=A%20prime%20number%20is%20a,the%20first%20few%20prime%20numbers.
+
 function isPrime(number) {
-
+  if (number === 1) {
+    return;
+  } else {
+    //loop from 2 to number-1
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
-
+console.log(isPrime(9));
+console.log(isPrime(2017));
