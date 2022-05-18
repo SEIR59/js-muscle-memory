22dbbd2e66a4950929f21183f03fb53b9726a5f5
andersons1218 <andersons1218@gmail.com>
2022-05-18 18:40:22 -0400

    3-is-prime.js

--- 3-is-prime.js
+++ 3-is-prime.js
@@ -12,6 +12,23 @@ isPrime(2017); // => true
 ***************************************************************************/
 
 function isPrime(number) {
-
+    if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
 }
-
+console.log(isPrime(37))
