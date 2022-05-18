6f174f8038726c72646bab30ec935fb1c13a8349
Mark Gerken <mgerken86@gmail.com>
2022-05-18 17:00:12 -0600

    7 complete

--- 7-adults.js
+++ 7-adults.js
@@ -15,6 +15,42 @@ const ppl = [
 adults(ppl); // => [ 'Khalid Robinson', 'Post Malone' ]
 ***********************************************************************/
 
-function adults(people) {
-  
+const ppl = [
+  {name: 'Khalid Robinson', age: 22},
+  {name: 'Ariel Winter', age: 20},
+  {name: 'Post Malone', age: 25},
+  {name: 'Willow Smith', age: 17}
+];
+
+//This **works**, but only return the objects of the people who are 18 and over. I want to try to return just the names
+// function adults(people) {
+//   const adultsArr = ppl.filter((person) => {
+//     if (person.age >= 18){
+//       return person
+//     }
+//   })
+// }
+
+//This ***works***, but still has an undefined El in the new array
+// const adults = (people) => {
+//   return adultsArr = ppl.map((person) => {
+//     if (person.age >= 18){
+//       return person.name
+//     } else {
+//       const index = ppl.indexOf(person)
+//        ppl.splice(index, 1)
+//     }
+//   })
+// }
+const adults = (people) => {
+  const adultsArr = []
+  for (let i=0; i<people.length; i++){
+    if (people[i].age >= 18){
+      adultsArr.push(people[i].name)
+    }
+  }
+  return adultsArr
 }
+
+
+console.log(adults(ppl))
