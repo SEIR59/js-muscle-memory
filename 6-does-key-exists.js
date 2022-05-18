88939b2c7cbc589a7cfa0e78dc23a1e6db8592cf
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-18 19:08:49 -0400

    6. does keys

--- 6-does-key-exists.js
+++ 6-does-key-exists.js
@@ -4,12 +4,16 @@ key and returns true if the key is inside the object and false if the
 key is not inside the object.
 
 Examples:
-
+*/
 const obj1 = {company: 'General Assembly', course: 'Software Engineering Immersive'}
-doesKeyExist(obj1, 'company'); // => true
-doesKeyExist(obj1, 'name'); // => false
-***********************************************************************/
+console.log(doesKeyExist(obj1, 'company')); // => true
+console.log(doesKeyExist(obj1, 'name')); // => false
 
 function doesKeyExist(obj, key) {
-
+    let keys = Object.keys(obj)
+    if(keys.includes(key)){
+        return true
+    } else {
+        return false
+    }
 }
