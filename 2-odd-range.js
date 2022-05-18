16850d01f326f8ab6d4b6e6f51d79d8e31d597c1
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-18 18:11:42 -0400

    2-odd-range

--- 2-odd-range.js
+++ 2-odd-range.js
@@ -9,5 +9,27 @@ oddRange(6); // => [ 1, 3, 5 ]
 ******************************************************************************/
 
 function oddRange(end) {
+    //will probably need a for if loop where you use i%2 !==0
+
+    //create the array you will need to push into
+    let newArray = []
+    // console.log("testing")
+    // console.log(end);
+   //
+    for (let i=1; i<= end; i++){
+        
+        if (i%2 !==0){
+            // console.log(i)
+            newArray.push(i);
+            // console.log(newArray);
+        } 
+        // console.log(newArray);
+        // console.log(end.length);
+    }
+    //don't forget to return the new array
+
+    return newArray;
 
 }
+
+console.log(oddRange(6));
