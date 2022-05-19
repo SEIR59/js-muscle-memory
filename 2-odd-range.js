e6e666dd6f96bde668829f16ff0b5f4c89b9d5bb
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-05-18 21:05:52 -0400

    2 done

--- 2-odd-range.js
+++ 2-odd-range.js
@@ -9,5 +9,22 @@ oddRange(6); // => [ 1, 3, 5 ]
 ******************************************************************************/
 
 function oddRange(end) {
-
-}
+    let a = [];
+     
+     if(end%2 !== 0){
+     for(let i=0; i <= Math.floor(end/2); i++){
+       a[i] = i*2+1;
+     }  
+     return a;
+     }
+     
+     else if(end%2 === 0){
+       for(let i=0; i <= ((end/2)-1); i++){
+       a[i] = i*2+1;
+     }  
+       return a;
+      
+     }
+     
+     
+   }
