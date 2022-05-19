54bb9530ff75031560e6e62f73bb984d7879830f
victortan <vtan8090@gmail.com>
2022-05-18 20:01:22 -0400

    Made Cats. I am now a cat mother

--- 4-cat-builder.js
+++ 4-cat-builder.js
@@ -11,6 +11,20 @@ const cat2 = catBuilder('Whiskers', 'rainbow', ['poptarts']);
 cat2; // => { name: 'Whiskers', color: 'rainbow', toys: [ 'poptarts' ] }
 ***********************************************************************/
 
-function catBuilder(name, color, toys) {
-
+// "Classes are in fact 'Special Functions'" -MDN Web
+// Create a class of Cat
+class Cat {
+    // Give the constructor parameters
+    constructor(name, color, toys){
+        // let the parameters be the instanced parameters
+        this.name = name
+        this.color = color
+        this.toys = toys
+    }
 }
+
+// Create a new cat
+const newCat = new Cat('Garfield', 'golden', ['scratching-post', 'yarn'])
+
+// Console log the new cat
+console.log(newCat)
