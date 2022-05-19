e60e4967966846d204c60f0b5b67127c7e4e1fb1
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-18 21:36:14 -0400

    9-decomposition

--- 9-decomposition.js
+++ 9-decomposition.js
@@ -19,8 +19,28 @@ isPrime(303212); // => false
 ***********************************************************************/
 
 function isPrime(number) {
-    
+    //need to determine if a number is a prime or not
+
+    //1 is not a prime number, eliminate 1
+    if (number === 1){
+        return false;
+// used this resource to help me understand how to test for the prime numbers: https://www.w3resource.com/javascript-exercises/javascript-function-exercise-8.php
+    }else if (number === 2) {
+        return true;
+    //    
+    }else {
+        for (i = 2; i< number; i++){
+            //if it can be divided by anything, then it's not a prime number
+            if(number % i ===0){
+                return false;
+            }else {
+                return true;
+            }
+        }
+    }
 }
+// console.log(isPrime(17));
+// console.log(isPrime(24));
   
 /***********************************************************************
 Using the `isPrime` function you made, write a function `firstNPrimes(n)`
@@ -34,8 +54,16 @@ firstNPrimes(4); // => [2, 3, 5, 7]
 ***********************************************************************/
 
 function firstNPrimes(n) {
-
+  let array = [];
+  for (let i = 0; array.length < n; i++) {
+    if (isPrime(i) === true) {
+      array.push(i);
+    }
 }
+return array
+}
+
+// console.log(firstNPrimes(4));
 
 /***********************************************************************
  Using `firstNPrimes`, write a function `sumOfNPrimes(n)` that returns
@@ -47,7 +75,14 @@ sumOfNPrimes(0); // => 0
 sumOfNPrimes(1); // => 2
 sumOfNPrimes(4); // => 17
 ***********************************************************************/
-
+//jordan helped me with this. I still don't understand this totally and need to review it more
 function sumOfNPrimes(n) {
-
+    let iHateThis = firstNPrimes(n)
+    function add(total, newNumber){
+        return total + newNumber
+    }
+    let sum = iHateThis.reduce(add)
+  return sum
 }
+
+console.log(sumOfNPrimes(4))
