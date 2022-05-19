07ee40c18fb825a883ff6abcd86018326d0a208c
patwyzlic <patrickwyzlic@gmail.com>
2022-05-18 20:31:27 -0400

    Commit 3 - Is-prime

--- 3-is-prime.js
+++ 3-is-prime.js
@@ -12,6 +12,21 @@ isPrime(2017); // => true
 ***************************************************************************/
 
 function isPrime(number) {
-
+    if(number === 1){
+        return false;
+    }else if(number === 2){
+        return true;
+    }else{
+        for(let i = 2; i < number; i++){
+            if(number % i === 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
-
+console.log(isPrime(2));
+console.log(isPrime(10));
+console.log(isPrime(11));
+console.log(isPrime(9));
+console.log(isPrime(2017));
