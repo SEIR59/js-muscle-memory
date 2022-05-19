c74c76590bdeddf815c0897bf8cd87c49c1abdd3
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-05-18 23:46:49 -0400

    6 done

--- 6-does-key-exists.js
+++ 6-does-key-exists.js
@@ -9,7 +9,21 @@ const obj1 = {company: 'General Assembly', course: 'Software Engineering Immersi
 doesKeyExist(obj1, 'company'); // => true
 doesKeyExist(obj1, 'name'); // => false
 ***********************************************************************/
+const obj1 = {company: 'General Assembly', course: 'Software Engineering Immersive'}
 
 function doesKeyExist(obj, key) {
-
+    //  let array =[];
+    //  array.push(obj[key])
+    //  console.log(array)
+    //  if (array.length === 0){
+    //      return false;
+    //  }else{
+    //      return true;
+    //  }
+    console.log(key in obj)
+    if (key in obj === true){
+        return true
+    }
+    else{ return false}
+    
 }
