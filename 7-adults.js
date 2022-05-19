7bdbee67382b8f3dff7812687842b6c21f337ed6
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-18 20:04:33 -0400

    7-adults

--- 7-adults.js
+++ 7-adults.js
@@ -14,7 +14,26 @@ const ppl = [
 
 adults(ppl); // => [ 'Khalid Robinson', 'Post Malone' ]
 ***********************************************************************/
+const ppl = [
+  {name: 'Khalid Robinson', age: 22},
+  {name: 'Ariel Winter', age: 20},
+  {name: 'Post Malone', age: 25},
+  {name: 'Willow Smith', age: 17}
+];
 
-function adults(people) {
-  
-}
+
+ //probably use .filter
+ function adults (tuna){
+    const oldPeople = tuna.filter(function(word) {
+      if (word.age > 18){
+        return true
+      } else {
+        return false
+      }
+    })
+  return oldPeople;
+  };
+   
+console.log(adults(ppl)); // => [ 'Khalid Robinson', 'Post Malone' ]
+
+//need if else to filter out those who are younger that 18
