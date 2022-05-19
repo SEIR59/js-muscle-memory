7051de961ab08bac0a6cad0b74397d6b525f28ec
annakoanna <anna.231991k@gmail.com>
2022-05-19 07:25:42 -0700

    first 5 questions

--- 3-is-prime.js
+++ 3-is-prime.js
@@ -12,6 +12,18 @@ isPrime(2017); // => true
 ***************************************************************************/
 
 function isPrime(number) {
+    
+    if (number === 1 || number === 0) {
+        return false;
+    } else  {
+        for(let i = 2; i < number; i++) {
+            if( number % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
+console.log(isPrime(7))
 
