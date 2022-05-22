bd121a10d38c59c880367eeb06bd5bbea4c3fed5
Freightscape <jordan@freightscape.net>
2022-05-21 20:50:12 -0400

    Task 4 Completed

--- task-4.js
+++ task-4.js
@@ -83,3 +83,127 @@ if (num > 5) {
     console.log('else')
 }//5
 
+let num = 5;
+if (num > 5) {
+    console.log('if')
+} else {
+    console.log('else')
+}//6
+
+let num = 5;
+if (num > 5) {
+    console.log('if')
+} else {
+    console.log('else')
+}//7
+
+let num = 5;
+if(num> 5) {
+    console.log('if')
+} else {
+    console.log('else')
+}//8
+
+let num = 5;
+if (num > 5) {
+    console.log('if')
+} else {
+    console.log('else')
+}//9
+
+let num = 5;
+if (num > 5) {
+    console.log('if')
+} else {
+    console.log('else')
+}//10
+
+let num = 0;
+if (num < 0) {
+    console.log('if');
+} else if (num > 0) {
+    console.log('else if')
+} else {
+    console.log('else')
+}//1
+
+let num = 0;
+if(num < 0) {
+    console.log('if')
+} else if (num > 0) {
+    console.log('else if')
+} else {
+    console.log('else')
+}//2
+
+let num = 0;
+if (num < 0) {
+    console.log('if')
+} else if (num > 0) {
+    console.log('else if')
+} else {
+    (console.log('else'))
+}//3
+
+let num = 0;
+if (num < 0) {
+    console.log('if')
+} else if (num > 0) {
+    console.log('else it')
+} else {
+    console.log('else')
+} //4
+
+let num = 0;
+if (num < 0) {
+    console.log('if')
+} else if (num > 0) {
+    console.log('else if')
+} else {
+    console.log('else')
+}//5
+
+let num = 0;
+if {num > 0} {
+    console.log('if')
+} else if (num > 0) {
+    console.log('else if')
+} else {
+    console.log('else')
+}//6
+
+let num = 0;
+if (num > 0) {
+    console.log('if')
+} else if (num > 0) {
+    console.log('else if')
+} else {
+    console.log('else')
+}//7
+
+let num = 0;
+if (num > 0){
+    console.log('if')
+} else if (num > 0){
+    console.log('else if')
+} else {
+    console.log('else')
+}//8
+
+let num = 0;
+if (num > 0){
+    console.log('if')
+} else if (num >0){
+    console.log('else if')
+} else {
+    console.log('else')
+}//9
+
+let num =0;
+if (num < 0){
+    console.log('if')
+} else if (num > 0){
+    console.log('else if')
+} else {
+    console.log('if')
+}//
---
+++ task-5.js
@@ -0,0 +1,181 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ' how are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+//1
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + ' how are you?'
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+//2
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + ' how are you?'
+    return msg
+}
+console.log(sayHello('boot camp prep'));
+//3
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + ' how are you?'
+    return msg
+}
+console.log(sayHello('bootcamp prep'));
+//4
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + ' how are you?'
+    return msg
+}
+console.log(sayHello('bootcamp prep'))
+//5
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + ' how are you?'
+    return msg
+}
+console.log(sayHello('bootcamp prep'))
+//6
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + ' how are you?'
+    return msg
+}
+console.log(sayHello('bootcamp prep'))
+//7
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + ' how are you?'
+    return msg
+}
+console.log('bootcamp prep')
+//8
+
+function sayHello (name) {
+    let msg = hello 'Hello, ' + name + ' how are you?'
+    return msg
+}
+console.log(sayHello('bootcamp prep'))
+//9
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + ' how are you?'
+    return msg
+}
+console.log(sayHello('bootcamp prep'))
+//10
+
+///////
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(1));
+
+function checkNumber(num) {
+    if (num > 0){
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+} 
+console.log(checkNumber(2));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(3));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positve';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(4))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(6))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(7))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(8))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(9))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(10))
