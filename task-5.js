7dfcd102ec691f6dc134c36ce935ef95604b16ac
Freightscape <jordan@freightscape.net>
2022-05-23 08:57:05 -0400

    Task 5 Completed

--- task-5.js
+++ task-5.js
@@ -179,3 +179,287 @@ function checkNumber(num) {
     }
 }
 console.log(checkNumber(10))
+
+/////
+
+function fizzBuzz1(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log (i);
+        } else if (i % 5 === 0 && i % 3 !==0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzBuzz2(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log(i);
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log(i)
+        }
+    }
+}
+
+function fizzBuzz3(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log(i);
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log(i)
+        }
+    }
+}
+
+function fizzBuzz4(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log(i);
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log(i)
+        }
+    }
+}
+
+function fizzBuzz5(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log(i);
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log(i)
+        }
+    }
+}
+
+function fizzBuzz6(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log(i);
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log(i)
+        }
+    }
+}
+
+function fizzBuzz7(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log(i)
+        } else if (i % 5 === 0 && i % 3 !== 0){
+            console.log(i)
+        }
+    }
+}
+
+function fizzBuzz8(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log(i)
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log(i)
+        }
+    }
+}
+
+function fizzBuzz9(max) {
+    for (let i = 0; i < max; i += 1) {
+
+        if (i % 3 === 0 && i % 5 !== 0){
+            console.log(i)
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log(i)
+        }
+    }
+}
+
+function fizzBuzz10(max) {
+    for (let i = 0; i < max; i +=1) {
+
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log(i)
+        } else if (i % 5 === 0 && i % 3 !==0) {
+            console.log(i)
+        }
+    }
+}
+
+////
+
+function evenCaps(sentence) {
+    let newSentence = "";
+
+    for (let i = 0; i <  sentence.length; i++){
+        let char = sentence[i];
+    
+
+    if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+    } else {
+        newSentence += char;
+    }
+}
+return newSentence;
+}//1
+
+function evenCAps(sentence) {
+    let newSentence = "";
+
+    for (let i = 0; i < sentence.length; i++){
+        let char = sentence[i];
+
+        if (i % 3 === 0) {
+            let capitalChar = char.toUpperCase()
+            newSentence += capitalChar;
+    } else {
+        newSentence += char
+    }
+    return newSentence
+}
+}//2
+
+function evenCAps(sentence) {
+    let newSentence = "";
+
+    for (let i = 0; i < sentence.length; i++){
+        let char = sentence[i];
+
+        if (i % 4 === 0) {
+            let capitalChar = char.toUpperCase()
+            newSentence += capitalChar;
+        } else {
+            newSentence += char;
+        }
+return newSentence
+    }
+}// 3
+
+function evenCAps (sentence) {
+    let newSentence = "";
+
+    for (let i =0; i < sentence.length; i++) {
+        let char = sentence[i];
+
+        if (i % 5 ===0) {
+            let capitalChar = char.toUpperCase()
+            newSentence += capitalChar;
+        } else {
+            newSentence += char;
+        }
+        return newSentence
+
+    }
+}// 4
+
+function evenCAps (sentence){
+    let newSentence ="";
+
+    for (let i = 0; i < sentence.length; i++) {
+
+        let char = sentence[i];
+
+        if (i % 6 === 0) {
+            let capitalChar = char.toUpperCase()
+            newSentence += capitalChar;
+        } else {
+            newSentence += char
+        }
+        return newSentence
+    }
+}//5
+
+function evenCAps (sentence) {
+    let newSentence = "";
+
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+
+        if (i % 7 === 0) {
+            let capitalChar =char.toUpperCase()
+            newSentence += capitalChar
+        } else {
+            newSentence += char
+        }
+        return newSentence
+    }
+}//6
+
+function evenCaps(sentence) {
+    let newSentence= "";
+
+    for (let i = o; i < sentence.length; i++) {
+        let char = sentence[i];
+
+        if (i % 8 === 0) {
+            let capitalChar = char.toUpperCase()
+            newSentence += capitalChar
+        } else {
+            newSentence += char
+        }
+        return {
+            newSentence 
+        }
+    }
+}// 7
+
+function evenCAps(sentence) {
+    let newSentence = ""
+
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+
+        if (i % 9 === 0) {
+            let capitalChar =char.toUpperCase()
+            newSentence += capitalChar
+        } else {
+            newSentence += char
+        } return {
+            newSentence
+        }
+    }
+}//8
+
+function evenCAps(sentence) {
+    let newSentence = ""
+
+    for (let i = 0; i < sentence.length; i++){
+        let char = sentence[i];
+
+        if (i % 9 === 0) {
+            let capitalChar = char.toUpperCase()
+            newSentence += capitalChar
+        } else {
+            newSentence += char
+        } return {
+            newSentence
+        }
+
+    }
+}//9
+
+function evenCAps(sentence) {
+    let newSentence = "";
+
+    for (let i = 0; i < sentence.length; i ++) {
+        let char = sentence[i];
+
+        if (i % 9 === 0) {
+            let capitalChar = char.toUpperCase()
+            newSentence += capitalChar
+        } else {
+            newSentence += char
+        } return {
+            newSentence
+        }
+    }
+}//10
+
